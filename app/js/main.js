05fe853498c66f87cdcbcda535a636cc87e56d86
ilyuhago <54542270+ilyuhago@users.noreply.github.com>
2021-01-15 01:14:36 +0300

    Сверстал header

---
+++ app/js/main.js
@@ -0,0 +1,8 @@
+$(function () {
+  $('.banner-section__slider').slick({
+    dots: true,
+    prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="/app/images/arrow-left.svg" alt="стрелка" ></button>',
+    nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="/app/images/arrow-right.svg" alt="стрелка" ></button>',
+       
+  });
+});
